6dc8c95597776d45e4bcec1b5cdd02b04b798496
Cuma KÖSE <turkmvc@gmail.com>
2024-06-15 20:41:29 +0300

    Add project files.

---
+++ FileSaveToFtpModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Nlabs.GenericFileSrevice;
+public class FileSaveToFtpModel
+{
+    public FileSaveToFtpModel(string ftpAddress, string userName, string password)
+    {
+        FtpAddress = ftpAddress;
+        UserName = userName;
+        Password = password;
+    }
+    public string FtpAddress { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
